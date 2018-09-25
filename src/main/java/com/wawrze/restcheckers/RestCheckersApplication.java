c02a4eeeb0797830c1222abba466a4304948c3e6
wawrze <mateusz.wawreszuk@gmail.com>
2018-09-25 23:06:24 +0200

    deploy to Heroku

--- src/main/java/com/wawrze/restcheckers/RestCheckersApplication.java
+++ src/main/java/com/wawrze/restcheckers/RestCheckersApplication.java
@@ -2,12 +2,19 @@ package com.wawrze.restcheckers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestCheckersApplication {
+public class RestCheckersApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         SpringApplication.run(RestCheckersApplication.class, args);
+    }*/
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RestCheckersApplication.class);
     }
 
 }
