b09fb8da2d99f0f10d7773a16db036e68dd8d879
wawrze <mateusz.wawreszuk@gmail.com>
2018-10-01 14:13:42 +0200

    fix comp-comp game try 3

--- src/main/java/com/wawrze/restcheckers/gameplay/userInterface/RestUI.java
+++ src/main/java/com/wawrze/restcheckers/gameplay/userInterface/RestUI.java
@@ -26,7 +26,6 @@ public class RestUI implements UserInterface {
         if(inQueue.isEmpty())
             return null;
         s = inQueue.poll();
-        System.out.println("FROM INQUEUE: " + s);
         s = s.toLowerCase();
         String[] result;
         for(String o : options) {
